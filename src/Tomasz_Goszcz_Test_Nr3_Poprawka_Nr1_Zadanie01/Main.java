b4c7c8449e95547f4987f71114b3ef898add4b49
Tomasz Goszcz <tom.goszcz@gmail.com>
2025-11-16 11:13:55 +0100

    wiadomość o zmianach

--- src/Tomasz_Goszcz_Test_Nr3_Poprawka_Nr1_Zadanie01/Main.java
+++ src/Tomasz_Goszcz_Test_Nr3_Poprawka_Nr1_Zadanie01/Main.java
@@ -27,6 +27,7 @@ public class Main {
         System.out.println("\nMamy, które urodziły bliźnięta: " + Mother.findMotherWithTwins(newbornList));
 
         System.out.println("DUPA");
+        System.out.println("adas");
 
 
     }
